51a3126e0b606a080700c2edb8b972b75823d899
Jake Lee Kennedy <jake@bodhi.io>
2016-06-21 18:13:20 +0300

    add clear completed

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   todos: [],
   incompleteTodos: Ember.computed.filterBy('todos', 'isCompleted', false),
-  
+
+  completeTodos: Ember.computed.filterBy('todos', 'isCompleted', true),
+
+  hasCompleted: Ember.computed.bool('completeTodos.length'),
+
   actions: {
     createTodo(content) {
       this.get('todos').addObject(this.store.createRecord('todo', {label: content}));
@@ -11,6 +15,12 @@ export default Ember.Controller.extend({
     destroyTodo(todo) {
       this.get('todos').removeObject(todo);
       todo.deleteRecord();
+    },
+    clearCompleted() {
+      this.get('todos').filterBy('isCompleted', true).forEach((todo) => {
+        this.get('todos').removeObject(todo);
+        todo.deleteRecord();
+      });
     }
   }
 });
