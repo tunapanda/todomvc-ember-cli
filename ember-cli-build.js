fd7244720b8bdf9694f32eda6691a10807a6592d
Jake Lee Kennedy <jake@bodhi.io>
2016-06-21 15:52:06 +0300

    initial commit

---
+++ ember-cli-build.js
@@ -0,0 +1,32 @@
+/*jshint node:true*/
+/* global require, module */
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var Funnel = require('broccoli-funnel');
+// var MergeTrees = require('broccoli-merge-trees');
+
+module.exports = function(defaults) {
+  var app = new EmberApp(defaults, {
+    // Add options here
+  });
+
+  var todomvcCSS = new Funnel('node_modules', {
+    include: ['todomvc-app-css/*.css', 'todomvc-common/*.css'],
+    destDir: 'assets/'
+  });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
+
+  // return new MergeTrees([todomvcCSS, app.toTree()]);
+  return app.toTree(todomvcCSS);
+};
