10f74979cac61a2496354ff98ae52711f3328532
Jake Lee Kennedy <jake@bodhi.io>
2016-06-21 17:57:00 +0300

    Add editing of todos

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -2,9 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
+  classNameBindings: ['isCompleted:completed', 'isEditing:editing'],
   actions: {
     toggleComplete() {
       this.toggleProperty('isCompleted');
+    },
+    editTodo() {
+      this.set('isEditing', true);
+    },
+    editingKeyDown(e) {
+      this.set('label', e.target.value);
+      if (e.keyCode === 13) {
+        this.set('isEditing', false);
+      }
+    },
+    doneEditing() {
+     this.set('isEditing', false); 
     }
   }
 });
