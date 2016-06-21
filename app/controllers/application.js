6ce1b56f8ff856da7d3255adfae1e826c5aba064
Jake Lee Kennedy <jake@bodhi.io>
2016-06-21 16:54:11 +0300

    Display the created 'Todos'

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
+  todos: [],
   actions: {
     createTodo(content) {
-      this.store.createRecord('todo', {label: content});
+      this.get('todos').addObject(this.store.createRecord('todo', {label: content}));
     }
   }
 });
